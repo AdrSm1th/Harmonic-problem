36ea1501bc7ea74863835062c6804a0845467981
sasha <sasha@DESKTOP-9BKVF17>
2026-04-21 17:38:49 +0700

    LOSsolver complete

--- Harmonic problem/iteration solver.cpp
+++ Harmonic problem/iteration solver.cpp
@@ -14,35 +14,35 @@ std::vector<BlockVector> LOSsolver::precondition(std::vector<Block> &M, std::vec
 	return result;
 }
 
-//double LOSsolver::dotProduct(std::vector<Block> &v1, std::vector<Block> &v2) const {
-//	double result = 0;
-//	int n = (int)v1.size();
-//
-//	for (int i = 0; i < n; result += v1[i].p_ * v2[i].p_ + v1[i].c_ * v2[i].c_);
-//
-//	return result;
-//}
-//
-//std::vector<Block> LOSsolver::VplusV(std::vector<Block> &v1, std::vector<Block> &v2) const {
-//	int n = (int)v1.size();
-//	std::vector<Block> result(n);
-//
-//	for (int i = 0; i < n; result[i] = v1[i] + v2[i], i++);
-//
-//	return result;
-//}
-//
-//std::vector<Block> LOSsolver::CmultV(double &c, std::vector<Block> &v) const {
-//	int n = (int)v.size();
-//	std::vector<Block> result(n);
-//
-//	for (int i = 0; i < n; result[i] = v[i] * c, i++);
-//
-//	return result;
-//}
+double LOSsolver::dotProduct(std::vector<BlockVector> &v1, std::vector<BlockVector> &v2) const {
+	double result = 0;
+	int n = (int)v1.size();
+
+	for (int i = 0; i < n; result += v1[i].p_ * v2[i].p_ + v1[i].c_ * v2[i].c_);
+
+	return result;
+}
+
+std::vector<BlockVector> LOSsolver::VplusV(std::vector<BlockVector> &v1, std::vector<BlockVector> &v2) const {
+	int n = (int)v1.size();
+	std::vector<BlockVector> result(n);
+
+	for (int i = 0; i < n; result[i] = v1[i] + v2[i], i++);
+
+	return result;
+}
+
+std::vector<BlockVector> LOSsolver::CmultV(double &c, std::vector<BlockVector> &v) const {
+	int n = (int)v.size();
+	std::vector<BlockVector> result(n);
+
+	for (int i = 0; i < n; result[i] = v[i] * c, i++);
+
+	return result;
+}
 
 void LOSsolver::solve(BlockCSRMatrix &matrix, std::vector<BlockVector> &b, std::vector<BlockVector> &x) {
-	int n = (int)b.size();
+	int n = (int)b.size(),i = 1;
 	std::vector<BlockVector> r(n), z(n), p(n);
 	std::vector<Block> M(n);
 	double a = 0, beta = 0, discrepancy = 1;
@@ -53,8 +53,31 @@ void LOSsolver::solve(BlockCSRMatrix &matrix, std::vector<BlockVector> &b, std::
 	}
 
 	r = precondition(M, b);
+	z = precondition(M, r);
+	std::vector<BlockVector> y(n);
+	matrix.multiply(z, y);
+	p = precondition(M, y);
 	
-	//for (int i = 0; i < n; i++) {
-	//	r[i].
-	//}
+	do
+	{
+		a = dotProduct(p, r) / dotProduct(p, p);
+		std::vector<BlockVector> az = CmultV(a, z);
+		x = VplusV(x, az);
+
+		discrepancy = sqrt(dotProduct(r, r) - (a * a) * dotProduct(p, p)) / sqrt(dotProduct(b, b));
+		double ma = -a;
+		std::vector<BlockVector> map = CmultV(ma, p);
+		r = VplusV(r, map);
+		std::vector<BlockVector> LAUr = precondition(M, r), t(n);
+		std::vector<BlockVector> t(n);
+		matrix.multiply(LAUr, t);
+		LAUr = precondition(M, t);
+
+		beta = dotProduct(p, LAUr) / dotProduct(p, p);
+		std::vector<BlockVector> pMr = precondition(M, r), betaz = CmultV(beta, z), betap = CmultV(beta, p);
+		z = VplusV(pMr, betaz);
+		p = VplusV(LAUr, betap);
+
+		i++;
+	} while (abs(discrepancy) > eps_ && i < maxiter_);
 }
--- Harmonic problem/iteration solver.h
+++ Harmonic problem/iteration solver.h
@@ -34,12 +34,13 @@
 class LOSsolver {
 private:
 	double eps_;
+	int maxiter_;
 	std::vector<BlockVector> precondition(std::vector<Block> &M, std::vector<BlockVector> &vector) const;
-	//double dotProduct(std::vector<Block> &v1, std::vector<Block> &v2) const;
-	//std::vector<Block> VplusV(std::vector<Block> &v1, std::vector<Block> &v2) const;
-	//std::vector<Block> CmultV(double &c, std::vector<Block> &v) const;
+	double dotProduct(std::vector<BlockVector> &v1, std::vector<BlockVector> &v2) const;
+	std::vector<BlockVector> VplusV(std::vector<BlockVector> &v1, std::vector<BlockVector> &v2) const;
+	std::vector<BlockVector> CmultV(double &c, std::vector<BlockVector> &v) const;
 
 public:
-	LOSsolver(double eps) : eps_(eps) {}
+	LOSsolver(double eps, int maxiter) : eps_(eps), maxiter_(maxiter) {}
 	void solve(BlockCSRMatrix &matrix, std::vector<BlockVector> &b, std::vector<BlockVector> &x);
 };
